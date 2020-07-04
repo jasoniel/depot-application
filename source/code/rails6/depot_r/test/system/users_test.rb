6ffc342e5c692c92cdaed752ee24dfc7287542dc
Jasoniel <jazoniel@gmail.com>
2020-07-04 13:02:17 -0300

    Depot Scaffold

---
+++ source/code/rails6/depot_r/test/system/users_test.rb
@@ -0,0 +1,55 @@
+#---
+# Excerpted from "Agile Web Development with Rails 6",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
+#---
+require "application_system_test_case"
+
+class UsersTest < ApplicationSystemTestCase
+  setup do
+    @user = users(:one)
+  end
+
+  test "visiting the index" do
+    visit users_url
+    assert_selector "h1", text: "Users"
+  end
+
+  test "creating a User" do
+    visit users_url
+    click_on "New User"
+
+    fill_in "Name", with: @user.name
+    fill_in "Password", with: 'secret'
+    fill_in "Password confirmation", with: 'secret'
+    click_on "Create User"
+
+    assert_text "User was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a User" do
+    visit users_url
+    click_on "Edit", match: :first
+
+    fill_in "Name", with: @user.name
+    fill_in "Password", with: 'secret'
+    fill_in "Password confirmation", with: 'secret'
+    click_on "Update User"
+
+    assert_text "User was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a User" do
+    visit users_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "User was successfully destroyed"
+  end
+end
