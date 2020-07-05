09ec8bcee7877433ae10f6c631d63bf7038b8e93
Jasoniel <jazoniel@gmail.com>
2020-07-05 19:29:00 -0300

    Creating a Smarter Cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -55,6 +55,7 @@ class ProductsController < ApplicationController
   # DELETE /products/1.json
   def destroy
     @product.destroy
+    puts @product.errors.full_messages
     respond_to do |format|
       format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
       format.json { head :no_content }
