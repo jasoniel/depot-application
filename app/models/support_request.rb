becf7bd852e7e3c21f51aa8bac2e54cc3cf7107b
Jasoniel <jazoniel@gmail.com>
2020-09-02 22:26:42 -0300

    Associates Support Request sent before the order

--- app/models/support_request.rb
+++ app/models/support_request.rb
@@ -1,5 +1,13 @@
 class SupportRequest < ApplicationRecord
-    belongs_to :order, optional: true
-
+    belongs_to :order, optional: true    
     has_rich_text :response
+
+    after_find :check_and_update_if_exists_orders
+
+    private
+
+    def check_and_update_if_exists_orders
+        last_order = Order.where(email: self.email).order(create_at: :desc).first
+        self.update(order: last_order) if last_order
+    end
 end
--- test/models/support_request_test.rb
+++ test/models/support_request_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class SupportRequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :support_requests
+
+  test "if support request associates sent before the purchase(order) " do    
+    order = Order.create(
+      name: "john test",
+      adress: "rua do john",
+      email: "john@teste.com",
+      pay_type: 0
+    )
+
+    support_request = SupportRequest.where(email: "john@teste.com").first
+    
+    assert_equal order, support_request.order
+  end
 end
